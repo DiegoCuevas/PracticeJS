f32b5e5c462bc1347db4c9327c22efa3216a9ef4
Diego Cuevas <diegocuevas579@gmail.com>
2022-02-21 23:48:21 -0500

    ADD: audio mario

--- event/main.js
+++ event/main.js
@@ -1,10 +1,11 @@
-document.getElementById("jumpButton").addEventListener("click", jump)
+/* document.getElementById("jumpButton").addEventListener("click", jump) */
 // document.getElementById("mario").addEventListener("mouseover", jump)
 // document.getElementById("mario").addEventListener("mouseout", jump)
 document.addEventListener("keyup", jump)
 function jump(){
   let mario = document.getElementById('mario')
+  let audio = document.getElementById('audio')
   mario.style.top = '300px'
+  audio.play();
   setTimeout(() => { mario.style.top = ''}, 500)
-  console.log("jump")
 }
